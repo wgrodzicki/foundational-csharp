c3c607725fc4ccb711780cc1f800d7a5232f3ffd
Rosynant <arrobasin@gmail.com>
2024-01-11 17:46:50 +0100

    first console app

---
+++ SimpleCsharpApps/FirstConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = System.Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
