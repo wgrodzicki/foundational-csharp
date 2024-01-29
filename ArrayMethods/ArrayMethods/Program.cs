37bfba75d152901c82f702da8fa95f1f28668ad5
Rosynant <arrobasin@gmail.com>
2024-01-29 18:39:04 +0100

    Reverse sentence challenge

---
+++ ArrayMethods/ArrayMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string newValue = new string(valueArray);
+string separatedValue = String.Join(":", valueArray);
+string[] splitValue = separatedValue.Split(":");
+
+Console.WriteLine(value);
+Console.WriteLine(newValue);
+Console.WriteLine(separatedValue);
+
+foreach (string element in splitValue)
+{
+    Console.WriteLine(element);
+}
---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] splitPangram = pangram.Split(" ");
+string newPangram = "";
+
+foreach (string item in splitPangram)
+{
+    char[] currentItemToChar = item.ToCharArray();
+    Array.Reverse(currentItemToChar);
+    string currentItemToString = new string(currentItemToChar);
+    newPangram += (currentItemToString + " ");
+}
+newPangram.TrimEnd();
+
+Console.WriteLine(newPangram);
