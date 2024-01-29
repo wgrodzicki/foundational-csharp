360ed3a216f31e08458df7cd5b02f1e673901fa7
Rosynant <arrobasin@gmail.com>
2024-01-29 17:55:04 +0100

    Data types

---
+++ Combining/Combining/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string alphabetical = "";
+decimal sum = 0.0m;
+
+foreach (String value in values)
+{
+    decimal currentNumber;
+    if (decimal.TryParse(value, out currentNumber))
+    {
+        sum += currentNumber;
+    }
+    else
+    {
+        alphabetical += value;
+    }
+}
+
+Console.WriteLine(alphabetical);
+Console.WriteLine(sum);
---
+++ Conversion/Conversion/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// ToString()
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
+
+// Helper method
+string third = "5";
+string fourth = "7";
+// int sum = int.Parse(third) + int.Parse(fourth);
+int thirdParsed = 0;
+int fourthParsed = 0;
+int sum = 0;
+
+if (int.TryParse(third, out thirdParsed) && int.TryParse(fourth, out fourthParsed))
+{
+    sum = thirdParsed + fourthParsed;
+}
+
+Console.WriteLine(sum);
+
+
+// Convert class - best to use for converting floats to ints (performs rounding)
+float value1 = 5.7f;
+float value2 = 7.6f;
+// string value1 = "5";
+// string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+
+
+// TryParse()
+string value = "102";
+int resultValue = 0;
+if (int.TryParse(value, out resultValue))
+{
+   Console.WriteLine($"Measurement: {resultValue}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+if (resultValue > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + resultValue}");
