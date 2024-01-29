bb8db1e84e713aeeaa627e27a028d26a254a901f
Rosynant <arrobasin@gmail.com>
2024-01-29 18:48:37 +0100

    Sort challenge

---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] splitOrderStream = orderStream.Split(",");
+Array.Sort(splitOrderStream);
+
+foreach (string order in splitOrderStream)
+{
+    if (order.Length != 4)
+    {
+        Console.WriteLine($"{order}\t- Error");
+    }
+    else
+    {
+        Console.WriteLine(order);
+    }
+}
