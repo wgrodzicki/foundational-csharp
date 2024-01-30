c10590e6c6ad5a0605960fa8aef13024889c153d
Rosynant <arrobasin@gmail.com>
2024-01-30 16:56:49 +0100

    String formattin

---
+++ Investment/Investment/Program.cs
@@ -0,0 +1,36 @@
+﻿
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.Write($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product");
+Console.WriteLine("that would dramatically increase your return.");
+Console.WriteLine();
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.");
+Console.WriteLine();
+Console.WriteLine($"Our new product, {newProduct}, offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.");
+Console.WriteLine();
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+comparisonMessage += currentProduct.PadRight(20);
+comparisonMessage += $"{currentReturn:P2}".PadRight(10);
+comparisonMessage += $"{currentProfit:C}".PadRight(20);
+
+comparisonMessage += ("\n" + newProduct.PadRight(20));
+comparisonMessage += $"{newReturn:P2}".PadRight(10);
+comparisonMessage += $"{newProfit:C}".PadRight(20);
+
+Console.WriteLine(comparisonMessage);
---
+++ StringFormatting/StringFormatting/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"\tInvoice Number: {invoiceNumber}");
+// Console.WriteLine($"\tProduct Shares: {productShares:N3}");
+// Console.WriteLine($"\tSubtotal: {subtotal:C}");
+// Console.WriteLine($"\tTax: {taxPercentage:P2}");
+// Console.WriteLine($"\tTotal: {total:C}");
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+string formattedLine = paymentId.PadRight(6,'-');
+formattedLine += payeeName.PadRight(24, '-');
+formattedLine += paymentAmount.PadLeft(10, '-');
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
