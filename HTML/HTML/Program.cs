fd0601af26fb6891bd1aa01f4349b0516e219268
Rosynant <arrobasin@gmail.com>
2024-01-30 18:28:38 +0100

    String helpers challenge

---
+++ HTML/HTML/Program.cs
@@ -0,0 +1,23 @@
+﻿
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string spanTagOpen = "<span>";
+const string spanTagClose = "</span>";
+const string divTagOpen = "<div>";
+const string divTagClose = "</div>";
+const string tm = "&trade";
+const string registered = "&reg";
+
+quantity = input.Substring(input.IndexOf(spanTagOpen) + spanTagOpen.Length);
+quantity = quantity.Remove(quantity.IndexOf(spanTagClose));
+
+output = input.Substring(input.IndexOf(divTagOpen) + divTagOpen.Length);
+output = output.Remove(output.IndexOf(divTagClose), divTagClose.Length);
+output = output.Replace(tm, registered);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
---
+++ StringHelpers/StringHelpers/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+
+// bool parenthesesPresent = false;
+
+// if (message.IndexOf('(') != -1)
+// {
+//     parenthesesPresent = true;
+// }
+
+// while (parenthesesPresent)
+// {
+//     if ((message.IndexOf('(') == -1) || (message.IndexOf(')') == -1))
+//     {
+//         parenthesesPresent = false;
+//     }
+//     else
+//     {
+//         message = message.Substring(message.IndexOf('(') + 1);
+//         Console.WriteLine(message.Remove(message.IndexOf(')')));
+//     }
+// }
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// //next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
