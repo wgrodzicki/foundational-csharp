e405bb4061b15154b55f18e7e71027ea5a24a4a4
Rosynant <arrobasin@gmail.com>
2024-02-04 11:33:11 +0100

    Methods parameters finished

--- Email/Email/Program.cs
+++ Email/Email/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+    DisplayEmail(corporate[i,0], corporate[i,1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+    DisplayEmail(external[i,0], external[i,1], externalDomain);
+}
+
+void DisplayEmail(string name, string surname, string domain = "contoso.com")
+{
+    string email = name.Remove(2).ToLower() + surname.ToLower() + "@" + domain;
+    Console.WriteLine(email);
+}
--- Email/Email/obj/Debug/net8.0/Email.AssemblyInfo.cs
+++ Email/Email/obj/Debug/net8.0/Email.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Email")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fbe6f0bfbcce25619e8ba37b00344c7d612825")]
 [assembly: System.Reflection.AssemblyProductAttribute("Email")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Email")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
