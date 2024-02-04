f31e677619b88a8d17eb900b3d6ad24f5701871e
Rosynant <arrobasin@gmail.com>
2024-02-04 14:09:40 +0100

    Validate a palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.Write("Enter the word: ");
+string? input = Console.ReadLine();
+
+if (String.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Invalid input.");
+}
+
+if (CheckPalindrome(input))
+{
+    Console.WriteLine($"{input} is a palindrome.");
+}
+else
+{
+    Console.WriteLine($"{input} is not a palindrome.");
+}
+
+bool CheckPalindrome(string input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != input[input.Length - 1 - i])
+        {
+            return false;
+        }
+
+        if (i == (input.Length / 2))
+        {
+            break;
+        }
+    }
+    return true;
+}
